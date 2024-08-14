33197a9281cd800baf9edba13ba9ff29882d7b14
Maxim Vasilyev <mayxis@inbox.ru>
2024-08-14 19:57:19 +0300

    chore: style and separate helpers

--- idu_api/urban_api/schemas/__init__.py
+++ idu_api/urban_api/schemas/__init__.py
@@ -69,16 +69,6 @@ from .territories import (
     TerritoryWithNormatives,
     TerritoryWithoutGeometry,
 )
-from .projects import (
-    Project,
-    ProjectPost,
-    ProjectPut,
-    ProjectPatch,
-    ProjectTerritory,
-    ProjectTerritoryPost,
-    ProjectTerritoryPut,
-    ProjectTerritoryPatch,
-)
 
 __all__ = [
     "PingResponse",
