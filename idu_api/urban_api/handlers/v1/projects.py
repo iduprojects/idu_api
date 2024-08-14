12f0ed3d5d05dddcc54f248367c1e5857e3d6f98
Maxim Vasilyev <mayxis@inbox.ru>
2024-08-14 19:55:51 +0300

    feat: add service logic

--- idu_api/urban_api/handlers/v1/projects.py
+++ idu_api/urban_api/handlers/v1/projects.py
@@ -2,38 +2,25 @@
 Projects endpoints are defined here.
 """
 
-from fastapi import Path, Query, Request
-from sqlalchemy.ext.asyncio import AsyncConnection
+from fastapi import Request
 from starlette import status
-from datetime import datetime
-from typing import List, Optional
 
-from idu_api.urban_api.logic.projects import (
-    get_projects_from_db,
-    post_project_to_db,
-    delete_project_from_db,
-    put_project_to_db,
-    patch_project_to_db,
-    get_project_by_id_from_db,
-    get_project_territory_by_id_from_db,
-)
-from idu_api.urban_api.schemas import Project, ProjectPost, ProjectPut, ProjectPatch, ProjectTerritory
-from idu_api.urban_api.schemas.geometries import Geometry
+from idu_api.urban_api.logic.projects import UserProjectService
+from idu_api.urban_api.schemas import Project, ProjectPatch, ProjectPost, ProjectPut, ProjectTerritory
 
 from .routers import projects_router
 
 
 @projects_router.get(
     "/projects",
-    response_model=List[Project],
+    response_model=list[Project],
     status_code=status.HTTP_200_OK,
 )
-async def get_projects(request: Request) -> List[Project]:
-    """
-    Get all projects
-    """
-    conn: AsyncConnection = request.state.conn
-    projects = await get_projects_from_db(conn)
+async def get_projects(request: Request) -> list[Project]:
+    """Get all projects."""
+    user_project_service: UserProjectService = request.state.user_project_service
+    projects = await user_project_service.get_projects_from_db()
+
     return [Project.from_dto(project) for project in projects]
 
 
@@ -43,11 +30,10 @@ async def get_projects(request: Request) -> List[Project]:
     status_code=status.HTTP_200_OK,
 )
 async def get_project_by_id(request: Request, project_id: int) -> Project:
-    """
-    Get project by id
-    """
-    conn: AsyncConnection = request.state.conn
-    project = await get_project_by_id_from_db(conn, project_id)
+    """Get a project by id."""
+    user_project_service: UserProjectService = request.state.user_project_service
+    project = await user_project_service.get_project_by_id_from_db(project_id)
+
     return Project.from_dto(project)
 
 
@@ -57,11 +43,10 @@ async def get_project_by_id(request: Request, project_id: int) -> Project:
     status_code=status.HTTP_200_OK,
 )
 async def get_projects_territory_info(request: Request, project_id: int) -> ProjectTerritory:
-    """
-    Get territory info of a project by id
-    """
-    conn: AsyncConnection = request.state.conn
-    project_territory_dto = await get_project_territory_by_id_from_db(conn, project_id)
+    """Get territory info of a project by id."""
+    user_project_service: UserProjectService = request.state.user_project_service
+    project_territory_dto = await user_project_service.get_project_territory_by_id_from_db(project_id)
+
     return ProjectTerritory.from_dto(project_territory_dto)
 
 
@@ -71,40 +56,36 @@ async def get_projects_territory_info(request: Request, project_id: int) -> Proj
     status_code=status.HTTP_201_CREATED,
 )
 async def post_project(request: Request, project: ProjectPost) -> Project:
-    """
-    Post a project
-    """
-    conn: AsyncConnection = request.state.conn
-    project_dto = await post_project_to_db(conn, project)
+    """Add a new project."""
+    user_project_service: UserProjectService = request.state.user_project_service
+    project_dto = await user_project_service.post_project_to_db(project)
+
     return Project.from_dto(project_dto)
 
 
 @projects_router.put(
     "/projects/{project_id}",
     response_model=Project,
-    status_code=status.HTTP_202_ACCEPTED,
+    status_code=status.HTTP_200_OK,
 )
 async def put_project(request: Request, project: ProjectPut, project_id: int) -> Project:
-    """
-    Put a project
-    """
-    conn: AsyncConnection = request.state.conn
-    project_dto = await put_project_to_db(conn, project, project_id)
+    """Update a project by setting all of its attributes."""
+    user_project_service: UserProjectService = request.state.user_project_service
+    project_dto = await user_project_service.put_project_to_db(project, project_id)
+
     return Project.from_dto(project_dto)
 
 
-# TODO
 @projects_router.patch(
     "/projects/{project_id}",
     response_model=Project,
-    status_code=status.HTTP_202_ACCEPTED,
+    status_code=status.HTTP_200_OK,
 )
 async def patch_project(request: Request, project: ProjectPatch, project_id: int) -> Project:
-    """
-    Patch a project
-    """
-    conn: AsyncConnection = request.state.conn
-    project_dto = await patch_project_to_db(conn, project, project_id)
+    """Update a project by setting given attributes."""
+    user_project_service: UserProjectService = request.state.user_project_service
+    project_dto = await user_project_service.patch_project_to_db(project, project_id)
+
     return Project.from_dto(project_dto)
 
 
@@ -112,9 +93,8 @@ async def patch_project(request: Request, project: ProjectPatch, project_id: int
     "/projects/{project_id}",
     status_code=status.HTTP_200_OK,
 )
-async def delete_project(request: Request, project_id: int) -> None:
-    """
-    Delete a project
-    """
-    conn: AsyncConnection = request.state.conn
-    await delete_project_from_db(conn, project_id)
+async def delete_project(request: Request, project_id: int) -> int:
+    """Delete a project."""
+    user_project_service: UserProjectService = request.state.user_project_service
+
+    return await user_project_service.delete_project_from_db(project_id)
