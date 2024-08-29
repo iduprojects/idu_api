bf32022df3cbc841d30d34ddb94a40d5b36cfcfc
Aleksei Sokol <kanootoko@gmail.com>
2024-08-29 12:52:53 +0300

    Version 1.6.2 (urban_api 0.14.2)
    Changes:
    - fix level autoset on territory insertion

---
+++ idu_api/common/db/migrator/versions/2024-08-29_74736f898ad6_fix_level_autoset.py
@@ -0,0 +1,94 @@
+# pylint: disable=no-member,invalid-name,missing-function-docstring,too-many-statements
+"""fix level autoset for public.territories_data
+
+Revision ID: 74736f898ad6
+Revises: dfb61815581e
+Create Date: 2024-08-29 12:17:32.562240
+
+"""
+from typing import Sequence, Union
+from textwrap import dedent
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "74736f898ad6"
+down_revision: Union[str, None] = "dfb61815581e"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # create trigger function
+
+    op.execute(
+        sa.text(
+            dedent(
+                """
+                CREATE FUNCTION public.trigger_update_inner_data_on_insert()
+                RETURNS trigger
+                LANGUAGE plpgsql
+                AS $function$
+                BEGIN
+                    NEW.level = (
+                        SELECT COALESCE(level, 0) + 1
+                        FROM territories_data
+                        WHERE territory_id = NEW.parent_id
+                    );
+
+                    RETURN NEW;
+                END;
+                $function$;
+                """
+            )
+        )
+    )
+
+    # create triggers
+
+    op.execute(
+        sa.text(
+            dedent(
+                """
+                CREATE TRIGGER update_inner_data_on_insert_trigger
+                BEFORE INSERT ON public.territories_data
+                FOR EACH ROW
+                EXECUTE PROCEDURE public.trigger_update_inner_data_on_insert();
+                """
+            )
+        )
+    )
+
+    # alter previous trigger
+
+    op.execute(
+        sa.text(
+            dedent(
+                """
+                CREATE OR REPLACE TRIGGER update_inner_data_on_update_trigger
+                BEFORE UPDATE ON public.territories_data
+                FOR EACH ROW
+                EXECUTE PROCEDURE public.trigger_update_inner_data_on_update();
+                """
+            )
+        )
+    )
+
+
+def downgrade() -> None:
+    op.execute(
+        sa.text(
+            dedent(
+                """
+                CREATE OR REPLACE TRIGGER update_inner_data_on_update_trigger
+                BEFORE INSERT OR UPDATE ON public.territories_data
+                FOR EACH ROW
+                EXECUTE PROCEDURE public.trigger_update_inner_data_on_update();
+                """
+            )
+        )
+    )
+    op.execute("DROP TRIGGER update_inner_data_on_insert_trigger ON public.territories_data")
+    op.execute("DROP FUNCTION public.trigger_update_inner_data_on_insert")
