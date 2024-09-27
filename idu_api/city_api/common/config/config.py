7bb699a1ae6eeb5b07a70d00b86b2cbe2ace4936
Konstantin Soldatov <kmsoldatov@gmail.com>
2024-09-27 15:55:00 +0300

    chore: config city types removal

--- idu_api/city_api/common/config/config.py
+++ idu_api/city_api/common/config/config.py
@@ -8,8 +8,6 @@ from dotenv import load_dotenv
 class Config:
     def __init__(self):
         load_dotenv(Path().absolute() / "city_api" / f".env.{os.getenv('APP_ENV')}")
-        with open(Path().absolute() / "idu_api" / "city_api" / "city_types.json", "r") as fin:
-            self.city_types: list[dict] = json.load(fin)
 
     @staticmethod
     def get(key: str) -> str:
