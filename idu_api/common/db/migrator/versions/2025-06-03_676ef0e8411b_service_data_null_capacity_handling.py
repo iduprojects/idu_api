e6bf78a6620d31bbf15774893123bc10146ebe48
Banakh Andrei Maksimovich <uuetsukeu@mail.ru>
2025-06-03 13:07:51 +0300

    feat: null capacity handling triggers in services_data

---
+++ idu_api/common/db/migrator/versions/2025-06-03_676ef0e8411b_service_data_null_capacity_handling.py
@@ -0,0 +1,88 @@
+# pylint: disable=no-member,invalid-name,missing-function-docstring,too-many-statements
+"""service data null capacity handling
+
+Revision ID: 676ef0e8411b
+Revises: e519990b4b4f
+Create Date: 2025-06-03 12:29:13.243421
+
+"""
+from textwrap import dedent
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "676ef0e8411b"
+down_revision: Union[str, None] = "e519990b4b4f"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # create handling null capacity function
+    op.execute(
+        sa.text(
+            dedent(
+                """
+                CREATE OR REPLACE FUNCTION public.trigger_set_capacity_from_modeled()
+                RETURNS trigger
+                LANGUAGE plpgsql
+                AS $function$
+                BEGIN
+                    IF NEW.capacity IS NULL THEN
+                        NEW.capacity := (
+                            SELECT capacity_modeled 
+                            FROM public.service_types 
+                            WHERE id = NEW.service_type_id
+                            LIMIT 1  -- Защита от дубликатов
+                        );
+                    END IF;
+                    RETURN NEW;
+                END;
+                $function$;
+                """
+            )
+        )
+    )
+
+    # create handling null capacity triggers
+    for trigger_name, table_name, procedure_name in [
+        (
+                "handling_null_capacity_trigger",
+                "public.services_data",
+                "public.trigger_set_capacity_from_modeled",
+        ),
+        (
+                "handling_null_capacity_trigger",
+                "user_projects.services_data",
+                "public.trigger_set_capacity_from_modeled",
+        ),
+    ]:
+        op.execute(
+            sa.text(
+                dedent(
+                    f"""
+                    CREATE TRIGGER {trigger_name}
+                    BEFORE INSERT OR UPDATE ON {table_name}
+                    FOR EACH ROW
+                    EXECUTE PROCEDURE {procedure_name}();
+                    """
+                )
+            )
+        )
+
+
+def downgrade() -> None:
+    # delete triggers
+    for trigger_name, table_name in [
+        ("handling_null_capacity_trigger", "public.services_data"),
+        ("handling_null_capacity_trigger", "user_projects.services_data"),
+    ]:
+        op.execute(sa.text(f"DROP TRIGGER {trigger_name} ON {table_name}"))
+
+    # delete function
+    for function_name in [
+        "public.trigger_set_capacity_from_modeled",
+    ]:
+        op.execute(sa.text(f"DROP FUNCTION {function_name}"))
